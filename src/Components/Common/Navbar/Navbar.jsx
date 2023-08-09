79c5190c8ab360d1d931d073db9ce59886a74c5e
AntyDeveloper <contact@aspdev.tech>
2023-08-09 19:42:44 +0200

    feat: burger changes

--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -1,12 +1,66 @@
 import "./Navbar.scss";
 import { MdSpaceDashboard } from "react-icons/md";
 import { useDisclosure } from "@mantine/hooks";
-import { Burger } from "@mantine/core";
+import { Burger, Transition, Paper, createStyles, rem } from "@mantine/core";
 import { HashLink as Link } from "react-router-hash-link";
+
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: 'relative',
+    zIndex: 1,
+  },
+
+  dropdown: {
+    position: 'absolute',
+    top: '60',
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopWidth: 0,
+    overflow: 'hidden',
+
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  link: {
+    display: 'block',
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+
+    [theme.fn.smallerThan('sm')]: {
+      borderRadius: 0,
+      padding: theme.spacing.md,
+    },
+  },
+
+}));
+
 
 const Navbar = () => {
   const [opened, { toggle }] = useDisclosure(false);
   const label = opened ? "Close navigation" : "Open navigation";
+  const { classes, cx } = useStyles();
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -26,7 +80,9 @@ const Navbar = () => {
         <Link to="/shop" smooth relative="path">
           Sklep
         </Link>
+        
       </div>
+      
       <div className="account">
         <a href="https://www.google.com">
           <div className="button">
@@ -41,6 +97,13 @@ const Navbar = () => {
           className="burger"
           color="#fff"
         />
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper className={classes.dropdown} withBorder style={styles}>
+            </Paper>
+          )}
+        </Transition>
+
       </div>
     </div>
   );
