0a05cee2179a00a46c4b7f6334f44f49485299ad
TheRoxer <xroxer.masterx@gmail.com>
2023-09-12 22:17:00 +0200

    fix: small fixes

--- src/Components/Common/Hero/Hero.jsx
+++ src/Components/Common/Hero/Hero.jsx
@@ -4,37 +4,37 @@ import { AiOutlineInfoCircle } from "react-icons/ai";
 import Card from "../Card/Card";
 
 const Hero = () => {
-  return (
-    <div className="hero-container">
-      <div className="hero-text">
-        <div className="hero-text-title">
-          <h1>
-            Starify <span>Development</span>
-          </h1>
-          <p>
-            Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            vulputate libero et velit interdum, ac aliquet odio mattis.
-          </p>
-        </div>
-        <div className="hero-text-buttons">
-          <a href="https://www.google.com">
-            <div className="button">
-              <AiOutlineInfoCircle className="icon" />
-              <p>Dowiedz się więcej</p>
-            </div>
-          </a>
-          <a href="https://dc.starify.tech">
-            <div className="social-button">
-              <SiDiscord size={24} />
-            </div>
-          </a>
-        </div>
-      </div>
-      <div className="hero-cards">
-        <Card path="./snippet.svg" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="hero-container">
+			<div className="hero-text">
+				<div className="hero-text-title">
+					<h1>
+						Starify <span>Development</span>
+					</h1>
+					<p>
+						Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+						vulputate libero et velit interdum, ac aliquet odio mattis.
+					</p>
+				</div>
+				<div className="hero-text-buttons">
+					<a href="#services">
+						<div className="button">
+							<AiOutlineInfoCircle className="icon" />
+							<p>Dowiedz się więcej</p>
+						</div>
+					</a>
+					<a href="https://dc.starify.tech">
+						<div className="social-button">
+							<SiDiscord size={24} />
+						</div>
+					</a>
+				</div>
+			</div>
+			<div className="hero-cards">
+				<Card path="./snippet.svg" />
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import NavBar from "../Components/Common/Navbar/Navbar";
 import Hero from "../Components/Common/Hero/Hero";
 import Footer from "../Components/Common/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Hero />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<Hero />
+			<Footer />
+			<Analytics />
+		</>
+	);
 }
 
 export default App;
