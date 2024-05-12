52bdb719122137edb8342ea782faa23461dad17c
AntyDeveloper <.>
2024-05-13 00:43:00 +0200

    fix background

--- src/Pages/Rules/Rules.jsx
+++ src/Pages/Rules/Rules.jsx
@@ -1,8 +1,5 @@
 import "./Rules.css";
-import NavBar from "../../Components/Common/Navbar/Navbar";
-import Footer from "../../Components/Common/Footer/Footer";
 
-import React from "react";
 
 function App() {	
 	return window.location.href = '/rules.pdf'
